6a9df4151d085b8d0f8afeee1311988cab5a3923
David Franklin <dave5801@gmail.com>
2018-04-04 14:21:25 -0700

    might be able to create a bucket

--- create_and_populate_s3_buckets.py
+++ create_and_populate_s3_buckets.py
@@ -39,6 +39,30 @@ class CreateNewS3Bucket(object):
     def __init__(self,url=None):
         self.url = url
         self.properties = S3BucketProperties(self.url)
+        list_of_aws_credentials = self.properties.get_aws_credentials()
+
+        '''
+         '''
+        resultant_object_from_s3_connection = boto.connect_s3(list_of_aws_credentials[0],
+            list_of_aws_credentials[1])
+           
+
+
+        bucket_name = self.properties.generate_unique_s3_bucket_name()
+
+        bucket = resultant_object_from_s3_connection.create_bucket(bucket_name,
+         location=boto.s3.connection.Location.DEFAULT)
+
+        print("BUCKET", bucket)
+        '''
+         '''
+        list_of_photos = self.properties.get_list_of_photos_from_local_directory()
+
+        for photo in list_of_photos:
+            a_photo_file = self.url + '/' + photo
+            k = Key(bucket)
+            k.key = photo
+            k.set_contents_from_filename(a_photo_file,cb=None, num_cb=10)
 
 if __name__ == '__main__':
     testfile = "nicholas_cage/"
